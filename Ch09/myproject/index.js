9bae6606776a9414d76e76a506ceb08261982625
Dean Lin <babydragon9703111@gmail.com>
2023-02-22 23:03:03 +0800

    更新

--- Ch09/myproject/index.js
+++ Ch09/myproject/index.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // 皆採用 .env 讀取
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-// 他套件使用方式錯誤，我直接改掉
+// 原案套件使用方式錯誤，我直接改掉
 const { Configuration, OpenAIApi } = require("openai"); 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -33,12 +33,12 @@ function handleEvent (event) {
     return Promise.resolve(null);
   }
 
-  return openai.createCompletion({ // 方法用錯修正
+  return openai.createCompletion({ // 調整成正確的使用方法
     prompt: event.message.text,
     model: "text-davinci-003", // 應用 model 而非 engine
     max_tokens: 1000 // 應用 max_tokens 而非 maxTokens
   }).then((completions) => {
-    // 原案少了 data 這層結構，並要加上 trim() 去除空白
+    // 原案少了 data 這層結構，還要加上 trim() 去除空白
     const message = completions.data.choices[0].text.trim();
     return client.replyMessage(event.replyToken, {
       type: 'text',
